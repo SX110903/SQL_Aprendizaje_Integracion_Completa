b1e84e0217f7b6ef1a44cd01d43b204358ab6e08
XS109 <xitaisong109@gmail.com>
2025-11-06 12:42:20 +0100

    primera parte del ejercicio

---
+++ ejercicio_joins.sql
@@ -0,0 +1,21 @@
+SELECT 
+    cliente.nombre_cliente AS nombre_cliente, 
+    empleado.nombre AS representante_ventas
+FROM cliente 
+INNER JOIN empleado ON cliente.codigo_empleado_rep_ventas = empleado.codigo_empleado;
+
+----Consulta 1.2: Mostrar los pedidos con los detalles del cliente que los realizó
+
+SELECT
+    cliente.nombre_cliente AS nombre_cliente,
+    pedido.codigo_pedido AS numero_pedido,
+    cliente.telefono AS telefono_cliente,
+    cliente.linea_direccion1 AS direccion_cliente_primaria,
+    cliente.codigo_postal AS codigo_postal_cliente,
+    cliente.region AS region_cliente,
+    cliente.pais AS pais_cliente,
+    cliente.fax AS fax_cliente
+FROM cliente 
+INNER JOIN pedido ON cliente.codigo_cliente = pedido.codigo_cliente;
+
+
